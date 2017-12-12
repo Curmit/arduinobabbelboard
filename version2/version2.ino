01c1bc2d0ccc623825fdc973f86e848527f6e950
Carmen <Carmen@wlan179154.mobiel.utwente.nl>
2017-12-12 18:32:36 +0100

    Version with for loops

--- version2/version2.ino
+++ version2/version2.ino
@@ -5,6 +5,10 @@ const int col1 = 12;// first column
 const int col2 = 8;// second column
 boolean status = true;
 
+const int numCols = 2;
+const int numRows = 2;
+
+
 
 
 void setup() {
@@ -16,7 +20,7 @@ void setup() {
   digitalWrite(input1, HIGH);
   digitalWrite(input2, HIGH);
   Serial.begin(9600);
-  
+
 
 
 }
@@ -26,44 +30,70 @@ void loop() {
   int reading = digitalRead(input1);
   int reading2 = digitalRead(input2);
 
-  //Put on column 1 on
-  pinMode(col1, OUTPUT);
-  pinMode(col2, INPUT);
 
 
-  //Check row 1 and row 2
-
-    //row 1
-    if (reading == LOW) {
-      Serial.println("Switch 1 is on, 0,0");
+  for (int i = 0; i < numCols; i++) {
+    Serial.println("ciao merde");
+    if (i == 0) {
+      //Put on column 1 on
+      pinMode(col1, OUTPUT);
+      pinMode(col2, INPUT);
     }
+    if ( i == 1) {
+      //Put on column 2 on
+      pinMode(col1, INPUT);
+      pinMode(col2, OUTPUT);
 
-    //row2
-    if (reading2 == LOW) {
-      Serial.println("switch2 is on 0,1");
-
-    }
- 
-  //Put on column 2 on
-  pinMode(col1, INPUT);
-  pinMode(col2, OUTPUT);
-  
-
- 
-    //row 1
-    if (reading == LOW) {
-      Serial.println("Switch 3 is on, 1,0");
-    }
-
-    //row2
-    if (reading2 == LOW) {
-      Serial.println("switch4 is on 1,1");
 
     }
-  
+    for (int j = 0; j < numRows; j++) {
+      Serial.println("rows");
+     
+
+      if (i == 0) {
+        //row 1
+        if (reading == LOW) {
+          Serial.println("Switch 1 is on, 0,0");
+        delay(1000);
+        break;
+        }
+
+        //row2
+        if (reading2 == LOW) {
+          Serial.println("switch2 is on 0,1");
+          delay(1000);
+          break;
+
+        }
+      }
+      else {
+        //row 1
+        if (reading == LOW) {
+          Serial.println("Switch 3 is on, 1,0");
+          delay(1000);
+          break;
+        }
+
+        //row2
+        if (reading2 == LOW) {
+          Serial.println("switch4 is on 1,1");
+          delay(1000);
+          break;
+
+        }
+
+      }
+
+      pinMode(col1, OUTPUT);
+      pinMode(col2, INPUT);
+    }
 
 
 
+
+
+
+  }
 
 }
 ///Serial.println(reading2);
